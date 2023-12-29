7f9b4e9aee83e3e523e94953f10ac63102581a01
pwojcieszak <piotrekw1002@gmail.com>
2023-12-29 01:44:23 +0100

    Added plane model and partially extracted data from multiple EJS files

---
+++ models/plane.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, '../data/planes.json');
+
+class Plane {
+  static getAllPlanes() {
+    try {
+      const planesData = fs.readFileSync(filePath, 'utf8');
+      const planes = JSON.parse(planesData);
+      return planes;
+    } catch (error) {
+      console.error('Error reading planes file:', error.message);
+      return [];
+    }
+  }
+
+  static getPlaneByName(planeName) {
+    let formattedPlaneName = planeName.charAt(0).toUpperCase() + planeName.slice(1);
+    console.log(formattedPlaneName);
+    const planes = Plane.getAllPlanes();
+    return planes.find(plane => plane.name === formattedPlaneName);
+  }
+}
+
+module.exports = Plane;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const Plane = require('../models/plane');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,7 +10,15 @@ router.get('/', function(req, res, next) {
 /* GET home page. */
 router.get('/:planeName', function(req, res, next) {
   let planeName = req.params.planeName;
-  res.render(planeName);
+  let currentPlane = Plane.getPlaneByName(planeName);
+  const planes = Plane.getAllPlanes();
+  console.log(currentPlane);
+
+  if (currentPlane) {
+    res.render('article', { currentPlane, planes });
+  } else {
+    res.status(404).send('Plane not found');
+  }
 });
 
 module.exports = router;
