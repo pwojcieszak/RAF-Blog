490896c32db21016b75308bc74f062de73dc1bcf
pwojcieszak <piotrekw1002@gmail.com>
2023-12-30 14:44:45 +0100

    Users can now post and delete theirs comments

--- models/comment.js
+++ models/comment.js
@@ -20,6 +20,43 @@ class Comment {
     const comments = Comment.getAllComments();
     return comments.filter(comment => comment.topic === formattedPlaneName);
   }
+
+  static addComment(content, nickname, topic) {
+    const comments = Comment.getAllComments();
+    comments.push({
+      content: content,
+      nickname: nickname,
+      topic: topic,
+    });
+
+    // Saving edited array
+    const jsonString = JSON.stringify(comments, null, 2);
+    fs.writeFile('data/comments.json', jsonString, 'utf8', (err) => {
+      if (err) {
+        console.error('Error writing JSON file:', err);
+        return true;
+      } else {
+        console.log('Array saved to JSON file successfully!');
+        return false;
+      }
+    });
+  }
+
+  static deleteCommentByDetails(content, nickname, topic) {
+    const comments = Comment.getAllComments();
+    const updatedComments = comments.filter(comment =>
+      comment.content !== content ||
+      comment.nickname !== nickname ||
+      comment.topic !== topic
+    );
+    try {
+      fs.writeFileSync(filePath, JSON.stringify(updatedComments, null, 2), 'utf8');
+      return true; 
+    } catch (error) {
+      console.error('Error writing to comments file:', error.message);
+      return false;
+    }
+  }
 }
 
 module.exports = Comment;
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const Plane = require('../models/plane');
 const planes = Plane.getAllPlanes();
 const Comment = require('../models/comment');
+const fs = require('fs');
 
 
 /* GET home page. */
@@ -41,12 +42,31 @@ router.get('/:planeName', function(req, res, next) {
 
   if (currentPlane) {
     const comments = Comment.getCommentsByName(planeName);
-    console.log(planeName);
-    console.log(comments);
     res.render('article', { currentPlane, planes, comments });
   } else {
     res.status(404).send('Plane not found');
   }
+});
+
+/* POST add comment. */
+router.post('/comments/post', function(req, res, next) {
+  const { content, nickname, topic } = req.body;
+  Comment.addComment(content, nickname, topic);
+  res.redirect('/' + topic);
+});
+
+/* POST delete comment. */
+router.post('/comments/delete', function(req, res, next) {
+  const { content, nickname, topic } = req.body;
+  console.log(content, nickname, topic);
+  const deletionSuccessful = Comment.deleteCommentByDetails(content, nickname, topic);
+  if (deletionSuccessful) {
+    console.log('Comment deleted successfully');
+  } else {
+    console.log('Error deleting comment');
+  }
+  let formattedTopic = topic.charAt(0).toLowerCase() + topic.slice(1);
+  res.redirect('/' + formattedTopic);
 });
 
 
